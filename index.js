ebd5c06708d667719d6f72c49106bcef10831cd8
Jeni Williams <stanfordjeni@gmail.com>
2017-09-16 14:28:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,7 +18,6 @@ function addToCart(item) {
 
 function viewCart() {
   // write your code here
-<<<<<<< HEAD
   var itemnum = cart.length
 
   if (itemnum >= 2) {
@@ -41,28 +40,6 @@ function viewCart() {
   } else {
     console.log("There is nothing in your cart.")
   }
-
-
-=======
-  var printout = []
-  var size = cart.length
-
-  for (var i = 0; i < size; i++) {
-    for (var x in cart[i]){
-      printout.push(x + " at $" + cart[i][x])
-    }
-  }
-
-  if (size >= 3) {
-    
-  } else if (size == 2) {
-    console.log("In your cart, you have " + printout[0] + " and " + printout[1] + ".")
-  } else if (size == 1) {
-    console.log("In your cart, you have " + printout.join() + ".")
-  } else {
-    console.log("Your shopping cart is empty.")
-  }
->>>>>>> 1d24fdbda1a5300e597b2a81a63dd3fd5a9a9b7c
 }
 
 function total() {
